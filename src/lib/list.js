4bb2873381be19fe4f1dd1144cc416375f85fff9
Karen <kpk3@hi.is>
2018-11-28 17:14:44 +0000

    m

---
+++ src/lib/list.js
@@ -0,0 +1,44 @@
+import { empty } from './helpers';
+
+export default class List {
+  constructor() {
+    this.container = document.querySelector('.list');
+    this.filters = document.querySelector('.meginmál__filterar');
+  }
+
+  loadLectures() {
+    fetch('../lectures.json')
+      .then((result) => {
+        if(!result.ok) {
+          throw new Error ('Non 200 status');
+        }
+        return result.json();
+      })
+      .then(data => console.log(data))
+      .catch(error => console.error(error));
+  }
+<<<<<<< HEAD
+=======
+
+  filterLectures() {
+    var 
+    if(!pressButton)
+
+  }
+
+  showLectures() {
+>>>>>>> Fridrik
+
+  filterLectures() {
+    
+  }
+
+
+
+  load() {
+    empty(this.container);
+    this.loadLectures()
+      .then(data => this.filterLectures(data))
+      .then(filter =)
+  }
+}
