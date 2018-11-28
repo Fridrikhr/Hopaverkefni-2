72cf69043d8e28ac77fd34f5d10540566e8f060b
Fridrihr <fhs6@hi.is>
2018-11-28 16:32:59 +0000

    push handa Almari

--- src/lib/list.js
+++ src/lib/list.js
@@ -3,10 +3,28 @@ import { empty } from './helpers';
 export default class List {
   constructor() {
     this.container = document.querySelector('.list');
-    this.filters =
+    this.filters = document.querySelector('.meginmál__filterar');
   }
 
   loadLectures() {
+    fetch('../lectures.json')
+      .then((result) => {
+        if(!result.ok) {
+          throw new Error ('Non 200 status');
+        }
+        return result.json();
+      })
+      .then(data => console.log(data))
+      .catch(error => console.error(error));
+  }
+
+  filterLectures() {
+    var 
+    if(!pressButton)
+
+  }
+
+  showLectures() {
 
   }
 
@@ -16,6 +34,6 @@ export default class List {
     empty(this.container);
     this.loadLectures()
       .then(data => this.filterLectures(data))
-      .then(filt =)
+      .then(filter =)
   }
 }
