19af42703881a50a1bf3d34e3768888360ca83b8
Fridrihr <fhs6@hi.is>
2018-11-28 12:21:36 +0000

    allt html gert að grunni

--- src/index.js
+++ src/index.js
@@ -4,7 +4,25 @@ document.addEventListener('DOMContentLoaded', () => {
   const page = document.querySelector('body');
   const isLecturePage = page.classList.contains('lecture-page');
 
-  if (isLecturePage) {
+  if (isLecturePage) { 
+    fetch('../lectures.json')
+      .then((result) => {
+        if(!result.ok) {
+          throw new Error ('Non 200 status');
+        }
+        return result.json();
+      })
+      .then(data => console.log(data))
+      .catch(error => console.error(error));
+    
+      const options = {
+        body: { /* object af post data */ },
+        cache: '', /* cache header */
+        headers: { /* auka headers */ },
+        method: 'POST',
+      };
+      
+      fetch('url', options);
 
   } else {
     const list = new List();
--- src/lib/list.js
+++ src/lib/list.js
@@ -3,9 +3,19 @@ import { empty } from './helpers';
 export default class List {
   constructor() {
     this.container = document.querySelector('.list');
+    this.filters =
   }
+
+  loadLectures() {
+
+  }
+
+
 
   load() {
     empty(this.container);
+    this.loadLectures()
+      .then(data => this.filterLectures(data))
+      .then(filt =)
   }
 }
