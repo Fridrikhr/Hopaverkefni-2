da052a75294fd4b6f73ba1a28ea336d18659643d
Fridrihr <fhs6@hi.is>
2018-11-29 19:02:32 +0000

    náði aftur í fyrri commit

--- src/lib/list.js
+++ src/lib/list.js
@@ -1,26 +1,43 @@
-import { empty } from './helpers';
+import { loadData } from './helpers';
 
-export default class List {
-  constructor() {
-    this.container = document.querySelector('.list');
-    this.filters = document.querySelector('.meginmál__filterar');
-  }
+function checkFunction(el) {
+  return el.className === 'category__button';
+}
 
-  loadLectures() {
-    fetch('../lectures.json')
-      .then((result) => {
-        if (!result.ok) {
-          throw new Error('Non 200 status');
-        }
-        return result.json();
-      })
-      .then(data => console.log(data))
-      .catch(error => console.error(error));
-  }
+function filterButtons(buttons) {
+  return buttons.filter(checkFunction);
+}
 
-  load() {
-    empty(this.container);
-    this.loadLectures()
-      .then(data => this.filterLectures(data));
+function onToggle(e) {
+  const element = e.srcElement;
+  element.classList.toggle('category__button--toggled');
+  loadData(false, document.querySelector('body'));
+}
+
+
+export function addListeners(page) {
+  const category = page.querySelector('.category__wrap');
+  let buttons = Array.from(category.childNodes);
+
+  buttons = filterButtons(buttons);
+
+  for (let i = 0; i < buttons.length; i += 1) {
+    buttons[i].addEventListener('click', onToggle);
   }
+}
+
+export function finishedListener(e) {
+  const slug = getSlug();
+  const lecture = e.path[1];
+  const img = lecture.firstChild;
+  const p = lecture.lastChild;
+  if (checkSlug(slug)) {
+    remove(slug);
+    p.textContent = 'Klára fyrirlestur';
+  } else {
+    add(slug);
+    p.textContent = 'Fyrirlestur kláraður';
+  }
+  img.classList.toggle('lecture__check--hidden');
+  lecture.classList.toggle('lecture__finish--green');
 }
